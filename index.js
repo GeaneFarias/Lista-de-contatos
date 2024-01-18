c90a106529dd55eac96ab244eb209a24d2e9f059
Geane Farias <fariassantos.geane@gmail.com>
2024-01-17 21:46:58 -0300

    Primeira etapa

---
+++ index.js
@@ -0,0 +1,29 @@
+const form = document.getElementById('form-contato')
+
+const Nome = []
+const numero = []
+
+let linhas = ''
+
+form.addEventListener('submit', function(e){
+    e.preventDefault();
+
+    const inputNomeContato = document.getElementById('nome-contato')
+    const inputNumContato = document.getElementById('num-contato')
+    
+
+    let linha = '<tr>';
+    linha += `<td> ${inputNomeContato.value}</td> `;
+    linha += `<td> ${inputNumContato.value} </td>`;
+    linha += '</tr>';
+
+   linhas += linha
+
+    const corpoTabela = document.querySelector('tbody');
+    corpoTabela.innerHTML = linhas
+
+   
+
+
+})
+
